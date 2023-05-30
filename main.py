0bfbe762551f36c78776f79bb26055424eac52c5
wasioujaharabi <wasiou.jaharabi@g.bracu.ac.bd>
2023-05-30 13:04:16 +0600

    with cors origin middleware

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from http.client import HTTPException
 from model import project
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 from database import (
     fetch_all_projects,
